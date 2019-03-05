054f35643741f508d582882a81b9edef6f1d8e0a
Kyle Decot <kyle@joinroot.com>
2019-03-05 01:33:02 -0500

    Requires version

--- lib/app_store_connect.rb
+++ lib/app_store_connect.rb
@@ -2,8 +2,9 @@ require "jwt"
 require "httparty"
 
 require "app_store_connect/authorization"
-require "app_store_connect/client"
 require "app_store_connect/cli"
+require "app_store_connect/client"
+require "app_store_connect/version"
 
 module AppStoreConnect
 end
