2caa81ce67768373d5438b1c1ac17c9ce6cc6a7e
Kyle Decot <kyle@joinroot.com>
2019-06-29 13:52:43 -0400

    Adds More Specs to Client

--- spec/app_store_connect/client_spec.rb
+++ spec/app_store_connect/client_spec.rb
@@ -13,6 +13,31 @@ RSpec.describe AppStoreConnect::Client do
     )
   end
 
+  describe '.app' do
+    let(:id) { '1234' }
+
+    before { subject.app(id) }
+
+    it_behaves_like :get_request, path: 'apps/1234'
+  end
+
+  describe '.builds' do
+    let(:id) { '1234' }
+
+    before { subject.builds(id) }
+
+    it_behaves_like :get_request, path: 'apps/1234/builds'
+  end
+
+  describe '.build' do
+    let(:app_id) { '1234' }
+    let(:build_id) { '5678' }
+
+    before { subject.build(app_id, build_id) }
+
+    it_behaves_like :get_request, path: 'apps/1234/builds/5678'
+  end
+
   describe '.apps' do
     before do
       subject.apps
