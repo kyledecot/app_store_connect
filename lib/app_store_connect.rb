85c371474005f54c585ce2e66c5ee6dad4cd7fb8
Kyle Decot <kyle@joinroot.com>
2019-07-03 19:49:15 -0400

    Moves Config to Class

--- lib/app_store_connect.rb
+++ lib/app_store_connect.rb
@@ -11,18 +11,10 @@ require 'app_store_connect/bundle_id_create_request'
 require 'app_store_connect/user_invitation_create_request'
 require 'app_store_connect/version'
 require 'app_store_connect/factory'
-
+require 'app_store_connect/config'
 require 'app_store_connect/type'
 require 'app_store_connect/type/enum'
 
 module AppStoreConnect
-  module Config
-    API = JSON.parse(
-      File.read(
-        File.join(__dir__, 'config/api.json')
-      )
-    )
-  end
-
   Parser.parse!(Config::API)
 end
--- lib/app_store_connect/config.rb
+++ lib/app_store_connect/config.rb
@@ -4,7 +4,7 @@ module AppStoreConnect
   module Config
     API = JSON.parse(
       File.read(
-        File.join(__dir__, 'config/api.json')
+        File.join(__dir__, '../config/api.json')
       )
     )
   end
