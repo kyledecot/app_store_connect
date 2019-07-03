c372155bb671bda42ade60625f6fcd2f9150c97d
Kyle Decot <kyle@joinroot.com>
2019-07-03 19:57:51 -0400

    Fix Formatting

--- spec/app_store_connect/config_spec.rb
+++ spec/app_store_connect/config_spec.rb
@@ -1,8 +1,10 @@
-RSpec.describe AppStoreConnect::Config do 
-  describe 'API' do 
+# frozen_string_literal: true
+
+RSpec.describe AppStoreConnect::Config do
+  describe 'API' do
     subject { described_class::API }
 
-    it { is_expected.to be_an_instance_of(Hash) } 
+    it { is_expected.to be_an_instance_of(Hash) }
     it { is_expected.to have_key('Type') }
-  end 
+  end
 end
