d66e845e25b0f0345aeb9079ac3a47ffe31ee6b4
Kyle Decot <kyle@joinroot.com>
2019-07-03 20:21:12 -0400

    Pass Parent in as Option

--- spec/app_store_connect/type/bundle_id_platform_spec.rb
+++ spec/app_store_connect/type/bundle_id_platform_spec.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 RSpec.describe AppStoreConnect::Type::BundleIdPlatform do
-  it_should_behave_like :type
+  it_should_behave_like :type, parent: AppStoreConnect::Type::Enum
 end
