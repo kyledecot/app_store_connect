1c8807b5b5831673401533fecf9df7419ff6a78d
Kyle Decot <kyle@joinroot.com>
2019-07-11 14:50:46 -0400

    Add spec for Object#to_h

---
+++ spec/app_store_connect/object_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe AppStoreConnect::Object do 
+  describe "#to_h" do 
+    it 'should return a hash' do 
+      klass = Class.new(described_class) do
+        property :string, {}
+        property :array, "array" => true
+      end 
+
+      object = klass.new(string: "foo")
+
+      expect(object.to_h).to eq({
+        string: "foo",
+        array: []
+      })
+    end 
+  end 
+end 
