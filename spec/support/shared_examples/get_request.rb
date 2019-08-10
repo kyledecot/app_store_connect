cddfa7a4a2d7c4c66ba83711385517d68b9e8a7f
Kyle Decot <kyle@joinroot.com>
2019-08-10 07:14:37 -0400

    rubocop

--- spec/support/shared_examples/get_request.rb
+++ spec/support/shared_examples/get_request.rb
@@ -4,9 +4,7 @@ RSpec.shared_examples :get_request do |path:, query_params: nil|
   it do
     uri = "https://api.appstoreconnect.apple.com/v1/#{path}"
 
-    if query_params
-      uri += "?#{query_params.to_query}"
-    end 
+    uri += "?#{query_params.to_query}" if query_params
 
     expect(WebMock).to have_requested(:get, uri)
   end
