d825e55e62ce7c2c1dc4b5f12924c5f4cec0f323
Kyle Decot <kyle@joinroot.com>
2019-08-20 19:08:33 -0400

    Accept kwargs

--- lib/app_store_connect/factory/builder/enum.rb
+++ lib/app_store_connect/factory/builder/enum.rb
@@ -4,8 +4,10 @@ module AppStoreConnect
   class Factory
     module Builder
       class Enum
-        def self.call(**_kwargs)
-          Class.new(Type::Enum)
+        def self.call(values:, **_kwargs)
+          Class.new(Type::Enum) do |base|
+            base.const_set('VALUES', values)
+          end
         end
       end
     end
