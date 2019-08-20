19deebaa3888120e83bd3e67363de6371c31d1e4
Kyle Decot <kyle@joinroot.com>
2019-08-20 19:43:27 -0400

    Inline parser

--- lib/app_store_connect.rb
+++ lib/app_store_connect.rb
@@ -3,8 +3,6 @@
 require 'jwt'
 
 require 'app_store_connect/authorization'
-require 'app_store_connect/parser'
-
 require 'app_store_connect/schema'
 require 'app_store_connect/object/type'
 require 'app_store_connect/object/attributes'
@@ -18,21 +16,28 @@ require 'app_store_connect/profile_create_request'
 require 'app_store_connect/certificate_create_request'
 require 'app_store_connect/user_invitation_create_request'
 require 'app_store_connect/version'
-
+require 'app_store_connect/factory'
 require 'app_store_connect/type'
 require 'app_store_connect/type/enum'
-
-require 'app_store_connect/factory'
 
 module AppStoreConnect
   @config = {}
 
   SCHEMA = Schema.new(File.join(__dir__, './config/schema.json'))
-  Parser.parse!(SCHEMA)
 
   class << self
-    attr_reader :config
+    attr_accessor :config
 
-    attr_writer :config
+    def load!
+      SCHEMA.types.each do |type_schema|
+        options = type_schema.options
+        klass = Factory.type(type_schema)
+        name = options[:type]
+
+        AppStoreConnect::Type.const_set(name, klass)
+      end
+    end
   end
+
+  load!
 end
