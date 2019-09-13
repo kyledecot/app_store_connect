f846f2e6129767e51b44f2b6191e1f80a29a1719
Kyle Decot <kyle@joinroot.com>
2019-09-13 17:59:23 -0400

    Add Schema::Object

--- lib/app_store_connect/schema.rb
+++ lib/app_store_connect/schema.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
 require 'app_store_connect/schema/type'
+require 'app_store_connect/schema/object'
 require 'app_store_connect/schema/web_service_endpoint'
 
 module AppStoreConnect
   class Schema
-    attr_reader :types, :web_service_endpoints
+    attr_reader :types, :web_service_endpoints, :objects
 
     def initialize(path)
       schema = JSON.parse(File.read(path)).deep_symbolize_keys
       @types = schema[:types].map do |options|
-        Type.new(**options)
+        Type.new(options)
       end
-      @web_service_endpoints = schema[:web_service_endpoints].map do |s|
-        WebServiceEndpoint.new(**s)
+      @web_service_endpoints = schema[:web_service_endpoints].map do |options|
+        WebServiceEndpoint.new(options)
+      end
+      @objects = schema[:objects].map do |options|
+        Object.new(options)
       end
     end
   end
