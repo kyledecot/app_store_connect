0076a51efd3caa5f33a1ff6eb202ee860f3fee34
Kyle Decot <kyle@joinroot.com>
2020-02-17 13:07:28 -0500

    Fix Formatting

--- lib/app_store_connect/client/utils.rb
+++ lib/app_store_connect/client/utils.rb
@@ -11,18 +11,18 @@ module AppStoreConnect
 
       # Right now this only supports gzip and json responses.
       # If you need to support a different type then add it.
-      def self.decode(string, content_type='application/json')
+      def self.decode(string, content_type = 'application/json')
         decoded_data = nil
 
         case content_type
         when 'application/a-gzip'
           sio = StringIO.new string
           gz = Zlib::GzipReader.new sio
-          decoded_data = gz.read()
+          decoded_data = gz.read
         else # Assume JSON
           decoded_data = JSON
-            .parse(string)
-            .deep_transform_keys { |k| k.underscore.to_sym }
+                         .parse(string)
+                         .deep_transform_keys { |k| k.underscore.to_sym }
         end
 
         decoded_data
