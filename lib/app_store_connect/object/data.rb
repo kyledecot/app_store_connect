ef80b51fac4fcec14b17eb1cfb6c1c8222ca31e6
Grzegorz Jakubiak <grzegorz@platogo.com>
2023-02-09 11:55:53 +0100

    Allow including resources in HTTP Body
    Co-authored-by: Tomislav Markanovic <tomislav@platogo.com>

--- lib/app_store_connect/object/data.rb
+++ lib/app_store_connect/object/data.rb
@@ -14,8 +14,10 @@ module AppStoreConnect
           include Object::Attributes
           include Object::Type
           include Object::Id
+          attr_reader :included
 
           data.send(:define_method, :initialize) do |**kwargs|
+            instance_variable_set('@included', kwargs.delete(:included).to_a.map(&:to_h))
             instance_variable_set('@relationships', kwargs.delete(:relationships).to_h)
             instance_variable_set('@attributes', data::Attributes.new(**kwargs))
             instance_variable_set('@id', kwargs[id_arg_name])
@@ -36,8 +38,9 @@ module AppStoreConnect
 
         def to_h
           {
-            data: data.to_h
-          }
+            data: data.to_h,
+            included: data.included
+          }.reject { |_k, v| v.blank? }
         end
       end
 
