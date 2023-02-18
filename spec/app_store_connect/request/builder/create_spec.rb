dcb761538a0cc733bb4af89ebdddc8c89845470d
= <kyle.decot@tabulous.xyz>
2023-02-18 22:16:17 +0800

    Trim mode

--- spec/app_store_connect/request/builder/create_spec.rb
+++ spec/app_store_connect/request/builder/create_spec.rb
@@ -3,6 +3,41 @@
 RSpec.describe AppStoreConnect::Request::Builder::Create do
   let(:create_request_builder) { described_class.new(type, properties) }
 
+  context 'with Review Submissions' do
+    let(:type) { 'reviewSubmissions' }
+    let(:properties) { %w[platform] }
+
+    describe '#url' do
+      subject { create_request_builder.url }
+
+      it { is_expected.to eq('https://api.appstoreconnect.apple.com/v1/reviewSubmissions') }
+    end
+
+    describe '#alias' do
+      subject { create_request_builder.alias }
+
+      it { is_expected.to eq('create_review_submission') }
+    end
+
+    describe '#source' do
+      subject { create_request_builder.source }
+
+      it { is_expected.to eq(<<~EXPECTED) }
+        require 'app_store_connect/create_request'
+
+        class ReviewSubmissionCreateRequest < CreateRequest
+          data do
+            type 'reviewSubmissions'
+
+            attributes do
+              property :platform
+            end
+          end
+        end
+      EXPECTED
+    end
+  end
+
   context 'with Bundle IDs' do
     let(:type) { 'bundleIds' }
     let(:properties) { %w[name identifier platform seed_id] }
