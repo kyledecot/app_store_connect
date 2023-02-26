36700bf57c3c7c354962fa2f3fe2c3e5ca5d9827
= <kyle.decot@tabulous.xyz>
2023-02-26 16:20:55 +0800

    Fix Merge

--- lib/app_store_connect/request/body.rb
+++ lib/app_store_connect/request/body.rb
@@ -5,11 +5,21 @@ module AppStoreConnect
     class Body
       def self.inherited(klass)
         super
+
+        klass.include(Object::Included)
         klass.include(Object::Data)
       end
 
       def initialize(**kwargs)
+        @included = self.class::Included.new([*kwargs.delete(:included)])
         @data = self.class::Data.new(**kwargs)
+      end
+
+      def to_h
+        {
+          data: data.to_h,
+          included: included.to_a
+        }
       end
     end
   end
