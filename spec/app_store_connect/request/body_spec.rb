f04a168eed8eeaf40da4ad8bf63284328a126fe7
Kyle Decot <kyle.decot@tabulous.xyz>
2023-02-27 00:32:25 +0800

    Add Spec for Request Body (#152)
    * Add Spec for Request Body
    
    * Cleanup spec

---
+++ spec/app_store_connect/request/body_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+RSpec.describe AppStoreConnect::Request::Body do
+  let(:klass) do
+    Class.new(described_class) do
+      include AppStoreConnect::Object::Data
+
+      data do
+        attributes do
+          property :bar
+        end
+      end
+    end
+  end
+
+  let(:instance) { klass.new(included: [{ type: :foo }], bar: :baz) }
+
+  subject { instance.to_h }
+
+  describe '#to_h' do
+    it { is_expected.to eq({ data: { attributes: { bar: :baz } } }) }
+  end
+end
